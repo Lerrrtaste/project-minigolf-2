12fa0de6857a1a33a4b4bbff7b955bf7cd659c3c
Lerrrtaste <lf2002@gmx.de>
2019-11-09 15:22:30 +0100

    User authentication via Login screen fully functional; Added error popup system

--- godotproject/helpers/game.gd
+++ godotproject/helpers/game.gd
@@ -1,9 +1,25 @@
 extends Node
 
+#general
+var room:Node
+var nk
+var nkr
+
+const NakamaRestClient = preload("res://addons/nakama-client/NakamaRestClient.gd")
+
+#user/nakama
+var username:String
+var user_id:String
+
+#server settings
+const HOST:String = "192.168.0.234"
+const PORT:int = 7350
+const KEY:String = "defaultkey"
+
+#game state
 enum GameStates {
 	INVALID = -1,
 	LOGIN = 10,
-	LOGINGUEST = 20,
 	MATCHLIST = 30,
 	LOBBY = 40,
 	PLAYING = 50
@@ -14,15 +30,69 @@ const GameStateScenesPath = {
 }
 
 var game_state = GameStates.INVALID
+
+#debugging
 var debugging := true
 
+
 func _ready() -> void:
-	pass
+	#start nakama rest
+	nk = NakamaRestClient.new()
+	nk.host = HOST
+	nk.port = PORT
+	nk.server_key = KEY
+	nk.use_ssl = false
+	nk.ssl_validate = false
+	nk.debugging = debugging
+	add_child(nk)
+	
+	#open login screen
+	game_state_change_to(GameStates.LOGIN,{})
 
-func game_state_change(new_state:int,args:Dictionary)->void:
+func check_promise(promise:Object)->bool:
+	if promise.response["data"].has("error"):
+		show_error(promise.response["data"]["code"],promise.response["data"]["message"],promise)
+		return false
+	return true
+
+func show_error(code:int = -1, message:String = "", promise:Object = null)->void:
+	###use template:
+	#if promise.response["data"].has("error"):
+	#	game.show_error(promise.response["data"]["code"],promise.response["data"]["message"],promise)
+	###
+	var inst = $ErrorPopup
+	inst.window_title = "Error code: %s"%(code if code != -1 else "generic (-1)")
+	inst.dialog_text = "There was an error (%s)."%code
+	if message != "":
+		inst.dialog_text = inst.dialog_text + "\nError message:\n" + message
+	if promise != null && debugging:
+		inst.dialog_text = inst.dialog_text + "\nPromise object response body:\n" + promise.response["body"]
+	inst.popup_centered()
+
+func game_state_change_to(new_state:int,args:Dictionary)->void:
 	if debugging: print("Changing state to GameState: ",get_game_state_name(new_state))
+	if GameStateScenesPath.keys().has(new_state):
+		if room != null:
+			room.queue_free()
+		room = load(GameStateScenesPath[new_state]).instance()
+		if room.has_method("initialize"):
+			room.initialize(args)
+		add_child(room)
+	game_state = new_state
+	_game_state_changed()
 
-func get_game_state_name(state:int)->String:
+func _game_state_changed()->void:
+	match game_state:
+		GameStates.LOGIN:
+			pass
+
+func get_nakama_rest_client()->Object:
+	return nk
+
+func get_nakama_rt_client()->Object:
+	return nkr
+
+static func get_game_state_name(state:int)->String:
 	match state:
 		-1: return "Inavlid"
 		10: return "Login"
---
+++ godotproject/rooms/login/login.gd
@@ -0,0 +1,54 @@
+extends Control
+
+onready var login_email = $TabContainer/Login/TxtEmail
+onready var login_password = $TabContainer/Login/TxtPassword
+onready var login_btn = $TabContainer/Login/BtnLogin
+onready var loginguest_username = $TabContainer/LoginGuest/TxtGuestUsername
+onready var loginguest_btn = $TabContainer/LoginGuest/BtnLoginGuest
+onready var register_username = $TabContainer/Register/TxtRegisterUsername
+onready var register_email = $TabContainer/Register/TxtRegisterEmail
+onready var register_password = $TabContainer/Register/TxtRegisterPassword
+onready var register_password_repeat = $TabContainer/Register/TxtRegisterPasswordRepeat
+onready var register_btn = $TabContainer/Register/BtnRegister
+onready var dbg_login = $DbgLogin
+
+var game
+var nk
+
+func _ready() -> void:
+	game = get_node("/root/Game")
+	nk = game.get_nakama_rest_client()
+	login_btn.connect("pressed",self,"_on_LoginBtn_pressed")
+	loginguest_btn.connect("pressed",self,"_on_LoginguestBtn_pressed")
+	register_btn.connect("pressed",self,"_on_RegisterBtn_pressed")
+	dbg_login.connect("pressed",self,"_on_DbgLogin_pressed")
+	if game.debugging:
+		dbg_login.visible = true
+		dbg_login.disabled = false
+
+func _on_LoginBtn_pressed()->void:
+	var promise = nk.authenticate_email(login_email.text, login_password.text)
+	yield(promise,"completed")
+	if game.check_promise(promise):
+		game.game_state_change_to(game.GameStates.MATCHLIST,{})
+
+func _on_LoginguestBtn_pressed()->void:
+	var promise = nk.authenticate_custom(loginguest_username.text+String(OS.get_ticks_usec()), true, "Guest_"+loginguest_username.text)
+	yield(promise,"completed")
+	if game.check_promise(promise):
+		game.game_state_change_to(game.GameStates.MATCHLIST,{})
+	#TODO check if duplicate username, then add random number at the end
+
+func _on_RegisterBtn_pressed()->void:
+	if register_password.text != register_password_repeat.text:
+		game.show_error(-1,"Passwords don't match")
+	var promise = nk.authenticate_email(register_email.text,register_password.text,true,register_username.text)
+	yield(promise,"completed")
+	if game.check_promise(promise):
+		game.game_state_change_to(game.GameStates.MATCHLIST,{})
+
+func _on_DbgLogin_pressed()->void:
+	var promise = nk.authenticate_custom("DEBUGUSER_"+String(OS.get_unix_time()),true,"DEBUGUSER_"+String(OS.get_unix_time()))
+	yield(promise,"completed")
+	if game.check_promise(promise):
+		game.game_state_change_to(game.GameStates.MATCHLIST,{})
